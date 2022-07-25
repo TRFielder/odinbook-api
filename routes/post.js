039ea6ba9370c3bc2658433be42965c2a5eb7c3d
Tom Fielder <tom.fielder97@gmail.com>
2022-07-25 19:01:46 +0100

    Add planned routes for post router

--- routes/post.js
+++ routes/post.js
@@ -15,4 +15,23 @@ router.get('/:id', (req, res) => res.send('NOT IMPLEMENTED: GET post by ID'));
 // POST new post
 router.post('/', (req, res) => res.send('NOT IMPLEMENTED: POST new post'));
 
+// POST comment on post by ID
+router.post('/:id', (req, res) => res.send('NOT IMPLEMENTED: POST new comment'));
+
+// ------------- PUT routes------------- //
+
+// PUT update post by ID
+router.put('/:id', (req, res) => res.send('NOT IMPLEMENTED: PUT update post by ID'));
+
+// PUT update comment by ID
+router.put('/:id/:comment', (req, res) => res.send('NOT IMPLEMENTED: PUT update comment by ID'));
+
+// ------------- DELETE routes------------- //
+
+// DELETE remove post by ID
+router.delete('/:id', (req, res) => res.send('NOT IMPLEMENTED: DELETE post by ID'));
+
+// DELETE comment by ID
+router.put('/:id/:comment', (req, res) => res.send('NOT IMPLEMENTED: DELETE comment by ID'));
+
 module.exports = router;
